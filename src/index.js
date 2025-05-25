ac0812dc26f80ee15a52c8d8a7ff960abfd2b6af
csarq <catherinesarquis@gmail.com>
2025-05-25 13:50:10 +0100

    add basic search behaviour and display val as city name

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("I'm linked");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  const searchInput = document.querySelector("#search-form-input");
+  const cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  console.log(searchInput.value);
+  //call the api
+  //search for the city
+}
+
+const searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", handleSearchSubmit);
